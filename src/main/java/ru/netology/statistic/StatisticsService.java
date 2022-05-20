bb9f3d22b506b4b9d749af84f85bf2f9c6e422f6
Анна <anakatkova166@yandex.ru>
2022-05-20 17:25:30 +0300

    New test

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -15,4 +15,22 @@ public class StatisticsService {
                 current_max = income;
         return current_max;
     }
+
+    public long findMin(long[] incomes) {
+        long current_min_index = 0;
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
+    }
+
+    public long maxWithOtherData(long[] incomes) {
+        long current_max = 0;
+        long maxWithOtherData = incomes[0];
+        for (long income : incomes)
+            if (maxWithOtherData < income)
+                maxWithOtherData = income;
+        return maxWithOtherData;
+    }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 3;
+
+        long actual = service.findMin(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxWithOtherData() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {4, 15, 8, 4, 3, 13, 8, 6, 11, 20, 12};
+        long expected = 20;
+
+        long actual = service.maxWithOtherData(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
