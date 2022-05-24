708af9e276bd90d78d8947f9ce859d5eaa4cb3d6
Анна <anakatkova166@yandex.ru>
2022-05-24 17:57:38 +0300

    Test for 100 percent

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,23 +10,10 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-
-
-    @Test
-    void maxWithOtherData() {
-        StatisticsService service2 = new StatisticsService();
-
         long[] incomesInBillions = {4, 15, 8, 4, 3, 13, 8, 6, 34, 45, 21};
         long expected = 45;
 
-        long actual = service2.maxWithOtherData(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
